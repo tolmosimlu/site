4965f35101326f35abff53992cd6061fd1895790
tolmosimlu <79552201+tolmosimlu@users.noreply.github.com>
2023-02-01 13:01:25 -0800

    Update file.php

--- file.php
+++ file.php
@@ -1,14 +1,15 @@
 <?php
-
+Header('Content-type: application/x-mpegURL');
+$url = $_GET['url'];
 $ch = curl_init();
 $timeout = 5; // set to zero for no timeout
 
-curl_setopt ($ch, CURLOPT_URL, "http://ifconfig.co/");
+curl_setopt ($ch, CURLOPT_URL, $url);
 
-curl_setopt($ch,CURLOPT_USERAGENT,'curl/7.19.7 (x86_64-redhat-linux-gnu) libcurl/7.19.7 NSS/3.44 zlib/1.2.3 libidn/1.18 libssh2/1.4.2');
-
+curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36');
+curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt ($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
 $file_contents = curl_exec($ch);
 curl_close($ch);
-echo $file_contents;
+die($file_contents);
 ?>

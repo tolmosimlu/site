541d308c6dc1bb5041d199d0edc25b8d164ae17e
tolmosimlu <79552201+tolmosimlu@users.noreply.github.com>
2023-02-03 12:39:58 -0800

    Update file.php

--- file.php
+++ file.php
@@ -8,6 +8,7 @@ curl_setopt ($ch, CURLOPT_URL, $url);
 
 curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36');
 curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt ($ch, CURLOPT_HEADER, 1);
 curl_setopt ($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
 $file_contents = curl_exec($ch);
 curl_close($ch);
